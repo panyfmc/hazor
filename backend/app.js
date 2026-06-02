2258b7dfd8892fa8d34bc2c71169e6a040dffa1e
panyfmc <paulavanessa010@gmail.com>
2026-06-02 13:40:00 -0300

    feat: define as rotas para novo aluno

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,13 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 const alunoRoutes = require('./src/routes/aluno-router')
+const igrejaRoutes = require('./src/routes/igreja-router')
+const regiaoRoutes = require('./src/routes/regiao-router')
+const grupoRouter = require('./src/routes/grupo-router')
 
 app.use('/api/alunos', alunoRoutes)
+app.use('/api/igrejas', igrejaRoutes)
+app.use('/api/regioes', regiaoRoutes)
+app.use('/api/grupos', grupoRouter)
 
 module.exports = app
