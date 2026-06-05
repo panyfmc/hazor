385a206605b757ada6a46a5b7493f33ffdec36c8
panyfmc <paulavanessa010@gmail.com>
2026-06-04 22:12:53 -0300

    feat: exibir alunos cadastrados na tela de inicio via card

--- frontend/src/app/features/alunos/components/aluno-card/aluno-card.ts
+++ frontend/src/app/features/alunos/components/aluno-card/aluno-card.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import {  CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core'
+import {  CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-aluno-card',
@@ -11,16 +11,15 @@ import {  CommonModule } from '@angular/common';
 })
 export class AlunoCard {
 
-  @Input() nome = '';
-  @Input() igreja = '';
-  @Input() regiao = '';
-  @Input() dataIngresso!: Date;
-  @Input() frequenciaAtual = 0;
-  @Input() frequenciaMeta = 0;
+  @Input() nomeCompleto = ''
+  @Input() igreja = ''
+  @Input() regiao = ''
+  @Input() dataIngresso = ''
+  @Input() grupo = ''
+  @Input() frequenciaAtual = 0
+  @Input() frequenciaMeta = 0
+  @Input() atividadesAtual = 0
+  @Input() atividadesMeta = 0
 
-  @Input() atividadesAtual = 0;
-  @Input() atividadesMeta = 0;
-
-  @Input() grupo: 'OFICINA' | 'MÍDIA' = 'OFICINA';
 
 }
