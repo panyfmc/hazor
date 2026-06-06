b89367de12f5a20eda37923b968388e1c10224de
panyfmc <paulavanessa010@gmail.com>
2026-06-06 12:58:30 -0300

    feat: add metodo put

--- frontend/src/app/core/services/aluno-service.ts
+++ frontend/src/app/core/services/aluno-service.ts
@@ -6,26 +6,19 @@ import { HttpClient } from '@angular/common/http'
 })
 
 export class AlunoService {
-
   private http = inject(HttpClient)
 
-  private apiUrl =
-    'http://localhost:3000/api/alunos'
+  private apiUrl = 'http://localhost:3000/api/alunos'
 
   listar() {
-
-    return this.http.get<any[]>(
-      this.apiUrl
-    )
-
+    return this.http.get<any[]>(this.apiUrl)
   }
 
   criar(aluno: any) {
+    return this.http.post(this.apiUrl, aluno)
+  }
 
-  return this.http.post(
-    'http://localhost:3000/api/alunos',
-    aluno
-  )
-
-}
+  atualizar(id: number, aluno: any) {
+    return this.http.put(`${this.apiUrl}/${id}`, aluno)
+  }
 }
