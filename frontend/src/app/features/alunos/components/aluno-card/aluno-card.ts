69fcd2f82bb9c5551e6a766ad94b5e2071bba8d2
panyfmc <paulavanessa010@gmail.com>
2026-06-06 13:04:00 -0300

    feat: add botao editar

--- frontend/src/app/features/alunos/components/aluno-card/aluno-card.ts
+++ frontend/src/app/features/alunos/components/aluno-card/aluno-card.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter, HostListener } from '@angular/core'
 import {  CommonModule } from '@angular/common'
 
 @Component({
@@ -20,6 +20,25 @@ export class AlunoCard {
   @Input() frequenciaMeta = 0
   @Input() atividadesAtual = 0
   @Input() atividadesMeta = 0
+  @Output() editarAluno = new EventEmitter<any>()
 
+  isMenuOpen = false
+
+  toggleMenu(event: Event) {
+    event.stopPropagation() // Evita fechar imediatamente pelo click do HostListener
+    this.isMenuOpen = !this.isMenuOpen
+  }
+
+  onEdit() {
+    this.isMenuOpen = false
+    // Dispara o evento passando o objeto do aluno com os dados originais
+    this.editarAluno.emit(this.aluno)
+  }
+
+  // Fecha o menu automaticamente se clicar em qualquer outro lugar da tela
+  @HostListener('document:click')
+  closeMenu() {
+    this.isMenuOpen = false
+  }
 
 }
