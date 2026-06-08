fba177c89bcb544e0bccbcfb88ab2fa62d785af8
panyfmc <paulavanessa010@gmail.com>
2026-06-08 00:14:52 -0300

    feat: put de alunos ajustado

--- backend/src/repositories/atualizar-aluno-repository.js
+++ backend/src/repositories/atualizar-aluno-repository.js
@@ -1,31 +1,21 @@
-const {sql} = require('../config/database')
+const { sql } = require('../config/database')
 
 async function atualizar(id, aluno) {
-
-    await new sql
-        .Request()
+    await new sql.Request()
         .input('id', sql.Int, id)
-        .input(
-            'nomeCompleto',
-            sql.NVarChar(150),
-            aluno.nomeCompleto
-        )
-        .input(
-            'igrejaId',
-            sql.Int,
-            aluno.igrejaId
-        )
-        .input(
-            'grupoId',
-            sql.Int,
-            aluno.grupoId
-        )
+        .input('nomeCompleto', sql.NVarChar(150), aluno.nomeCompleto)
+        .input('igrejaId', sql.Int, aluno.igrejaId)
+        .input('grupoId', sql.Int, aluno.grupoId)
+        .input('ativo', sql.Bit, aluno.ativo) 
+        .input('dataInativacao', sql.Date, aluno.dataInativacao ? aluno.dataInativacao : null)
         .query(`
             UPDATE Alunos
             SET
                 NomeCompleto = @nomeCompleto,
                 IgrejaId = @igrejaId,
-                GrupoId = @grupoId
+                GrupoId = @grupoId,
+                Ativo = @ativo,
+                DataInativacao = @dataInativacao
             WHERE Id = @id
         `)
 }
