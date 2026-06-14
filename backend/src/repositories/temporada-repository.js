ffdad993c728b2d70af70d1a1cd3fded4250e21b
panyfmc <paulavanessa010@gmail.com>
2026-06-13 22:39:38 -0300

    feat: repository da temporada

---
+++ backend/src/repositories/temporada-repository.js
@@ -0,0 +1,51 @@
+const {sql} = require('../config/database')
+
+async function listar() {
+    const result = await new sql.Request()
+    .query(`
+        SELECT *
+        FROM Temporadas
+        ORDER BY DataInicio DESC
+    `)
+    return result.recordset
+}
+
+async function criar(temporada) {
+    await new sql.Request()
+        .input('nome', sql.VarChar, temporada.nome)
+        .input('inicio', sql.Date, temporada.dataInicio)
+        .input('fim', sql.Date, temporada.dataFim)
+        .query(`
+            UPDATE Temporadas
+            SET Ativa = 0
+            WHERE Ativa = 1;
+            INSERT INTO Temporadas
+            (
+                Nome,
+                DataInicio,
+                DataFim
+            )
+            VALUES
+            (
+                @nome,
+                @inicio,
+                @fim
+            )
+        `)
+}
+
+async function buscarAtiva() {
+    const result = await new sql.Request()
+    .query(`
+        SELECT * 
+        FROM Temporadas
+        WHERE Ativa = 1    
+    `)
+    return result.recordset[0]
+}
+
+module.exports = {
+    listar,
+    criar,
+    buscarAtiva
+}
