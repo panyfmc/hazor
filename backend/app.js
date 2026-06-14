dc7b8dee2386054555278d4f0ab8821da11588ee
panyfmc <paulavanessa010@gmail.com>
2026-06-13 23:30:32 -0300

    feat: rotas base do app

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,17 @@ const alunoRoutes = require('./src/routes/aluno-router')
 const igrejaRoutes = require('./src/routes/igreja-router')
 const regiaoRoutes = require('./src/routes/regiao-router')
 const grupoRouter = require('./src/routes/grupo-router')
+const temporadaRouter = require('./src/routes/temporada-router')
+const aulaRouter = require('./src/routes/aula-router')
+const atividadeRouter = require('./src/routes/atividade-router')
 
-app.use('/api/alunos', alunoRoutes)
-app.use('/api/igrejas', igrejaRoutes)
-app.use('/api/regioes', regiaoRoutes)
-app.use('/api/grupos', grupoRouter)
+app.use('/alunos', alunoRoutes)
+app.use('/igrejas', igrejaRoutes)
+app.use('/regioes', regiaoRoutes)
+app.use('/grupos', grupoRouter)
+app.use('/temporadas', temporadaRouter)
+app.use('/aulas', aulaRouter)
+app.use('/atividades', atividadeRouter)
+
 
 module.exports = app
