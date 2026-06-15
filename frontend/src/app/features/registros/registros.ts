18774d3c327e6b895819acf3107f89951d906039
panyfmc <paulavanessa010@gmail.com>
2026-06-14 23:23:54 -0300

    feat: add signal para resolver o change detection

--- frontend/src/app/features/registros/registros.ts
+++ frontend/src/app/features/registros/registros.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, inject, OnInit, signal } from '@angular/core' // signal pra resolver o problema de Detecção de Mudanças (Change Detection)
+import { temporadaService } from '../../core/services/temporada-services'
 
 @Component({
   selector: 'app-registros',
   standalone: true,
   templateUrl: './registros.html'
 })
-export class Registros {
-
+export class Registros implements OnInit {
+  private temporadaService = inject(temporadaService)
+  // Inicializa a temporada como um Signal
+  temporada = signal<any>(null)
+  ngOnInit() {
+    this.temporadaService.buscarAtiva().subscribe(res => {
+      this.temporada.set(res)
+    })
+  }
 }
