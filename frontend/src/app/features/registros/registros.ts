a57fa937650ea71efeacfa0deb26c858d4f329ab
panyfmc <paulavanessa010@gmail.com>
2026-06-24 12:17:45 -0300

    feat: exportacao de dados reais no card histórico de oficinas

--- frontend/src/app/features/registros/registros.ts
+++ frontend/src/app/features/registros/registros.ts
@@ -1,18 +1,43 @@
-import { Component, inject, OnInit, signal } from '@angular/core' // signal pra resolver o problema de Detecção de Mudanças (Change Detection)
+import { Component, inject, OnInit, signal, effect, computed } from '@angular/core' // signal pra resolver o problema de Detecção de Mudanças (Change Detection)
 import { temporadaService } from '../../core/services/temporada-services'
+import { aulaService } from '../../core/services/aulas-services'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-registros',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './registros.html'
 })
 export class Registros implements OnInit {
   private temporadaService = inject(temporadaService)
+  private aulaService = inject(aulaService)
   // Inicializa a temporada como um Signal
   temporada = signal<any>(null)
+  oficinas = signal<any[]>([])
+
+  limiteExibicao = computed(() => {
+    return this.oficinas().slice(0, 5)
+  })
+
+  constructor() {
+    effect(() => {
+      const tempId = this.temporada()?.Id
+      if(tempId) {
+        this.carregarAulas(tempId)
+      }
+    })
+  }
+
   ngOnInit() {
     this.temporadaService.buscarAtiva().subscribe(res => {
       this.temporada.set(res)
+    })
+  }
+
+  private carregarAulas(temporadaId: number) {
+    this.aulaService.listarAulas(temporadaId).subscribe(res => {
+      this.oficinas.set(res) // Alimenta o signal das oficinas
     })
   }
 }
