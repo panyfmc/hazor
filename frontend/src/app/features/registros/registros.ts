91a131a87e0f90246f00cf41c503663a6902fa61
panyfmc <paulavanessa010@gmail.com>
2026-06-24 17:23:27 -0300

    feat: efeito no botao editar oficina

--- frontend/src/app/features/registros/registros.ts
+++ frontend/src/app/features/registros/registros.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit, signal, effect, computed } from '@angular/core' // signal pra resolver o problema de Detecção de Mudanças (Change Detection)
+import { Component, inject, OnInit, signal, effect, computed, HostListener } from '@angular/core' // signal pra resolver o problema de Detecção de Mudanças (Change Detection)
 import { temporadaService } from '../../core/services/temporada-services'
 import { aulaService } from '../../core/services/aulas-services'
 import { CommonModule } from '@angular/common'
@@ -20,6 +20,8 @@ export class Registros implements OnInit {
   temporada = signal<any>(null)
   oficinas = signal<any[]>([])
   alunos = signal<any[]>([])
+  menuAbertoId = signal<number | null>(null)
+
 
   limiteExibicao = computed(() => {
     return this.oficinas().slice(0, 5)
@@ -52,12 +54,22 @@ export class Registros implements OnInit {
   }
 
   carregarAlunos() {
-      this.alunoService.listar().subscribe({
-        next: (dados: any[]) => {
-          const mapeado = dados.map(AlunoMapper.fromApi)
-          this.alunos.set(mapeado) 
-        },
-        error: (erro) => console.error(erro)
-      })
+    this.alunoService.listar().subscribe({
+      next: (dados: any[]) => {
+        const mapeado = dados.map(AlunoMapper.fromApi)
+        this.alunos.set(mapeado) 
+      },
+      error: (erro) => console.error(erro)
+    })
+  }
+
+  @HostListener('document:click')
+    fecharMenus() {
+        this.menuAbertoId.set(null)
+    }
+
+    alternarMenu(id: number, event: Event) {
+        event.stopPropagation()
+        this.menuAbertoId.set(this.menuAbertoId() === id ? null : id)
     }
 }
