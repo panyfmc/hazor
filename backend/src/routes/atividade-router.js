a77c2ed9190d985d77306fcdcddd6eafebe3e12e
panyfmc <paulavanessa010@gmail.com>
2026-08-05 17:15:59 -0300

    refactor: ajuste na ordem das requisicoes

--- backend/src/routes/atividade-router.js
+++ backend/src/routes/atividade-router.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const controller = require('../controllers/atividade-controller')
 
 router.post('/', controller.criar)
+router.post('/:atividadeId/entregas', controller.criarEntrega)
 router.get('/oficina/:oficinaId', controller.buscarPorOficina)
-router.delete('/:id', controller.excluir)
+router.get('/:atividadeId/entregas', controller.listarPorAtividade)
 router.put('/:atividadeId/entregas', controller.editarEntregas)
 router.patch('/:id/encerrar', controller.encerrar)
-router.get('/:atividadeId/entregas', controller.listarPorAtividade)
-router.post('/:atividadeId/entregas', controller.criarEntrega)
+router.delete('/:id', controller.excluir)
 router.delete('/:atividadeId/entregas/:alunoId', controller.removerEntrega)
 
 module.exports = router
